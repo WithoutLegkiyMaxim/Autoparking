da041c5829938edc01910fa6d7aa43985d07564d
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-21 22:58:19 +0800

    Init commit

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./log/entrypoint
+echo '' > ./log/entrypoint/npm_log
+echo '' > ./log/entrypoint/yarn_log
+echo '' > ./log/entrypoint/bundle_log
+npm rebuild 2>./log/entrypoint/npm_log && yarn install 2>./log/entrypoint/yarn_log
+rm ./tmp/pids/server.pid >/dev/null
+bundle 2>./log/entrypoint/bundle_log
+rails webpacker:install
+rails webpacker:install:react
+foreman start
---
+++ serverUp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo chmod -R 777 ./pgdata
+docker-compose up --build
