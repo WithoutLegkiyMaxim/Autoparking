fa2f8673bf3dc319f7bf93259f4e34bd2102fb03
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-23 13:57:35 +0800

    Fix path for create missing dir

--- serverUp.sh
+++ serverUp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir /usr/local/var/postgres/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/
-touch /usr/local/var/postgres/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/.keep
+mkdir ./pgdata/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/
+touch ./pgdata/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/.keep
 sudo chmod -R 777 ./pgdata
 docker-compose up --build
