dc7ed63ddd3bd3f4c92bb3d2137436c62c54c479
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-23 13:58:26 +0800

    Fuck

--- serverUp.sh
+++ serverUp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+sudo chmod -R 777 ./pgdata
 mkdir ./pgdata/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/
 touch ./pgdata/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/.keep
-sudo chmod -R 777 ./pgdata
 docker-compose up --build
