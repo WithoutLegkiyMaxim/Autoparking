ded8990720e76bcdebe68f11ad1d6da6ebb53f7c
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-23 13:51:29 +0800

    Add createing missing folders and dummys as .keep in pgdata folder, fix pg-container

--- serverUp.sh
+++ serverUp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir /usr/local/var/postgres/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/
+touch /usr/local/var/postgres/{pg_tblspc,pg_twophase,pg_stat,pg_stat_tmp,pg_replslot,pg_snapshots}/.keep
 sudo chmod -R 777 ./pgdata
 docker-compose up --build
