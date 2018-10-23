6dedac334977e67658cbbc25b312b202ddb43587
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-23 14:54:31 +0800

    Hurt Me

--- serverUp.sh
+++ serverUp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo chmod -R 770 ./pgdata
-mkdir ./pgdata/pg_commit_ts/
+sudo chmod -R 777 ./pgdata
+if ! test -d "./pgdata/pg_commit_ts"; then
+  mkdir ./pgdata/pg_commit_ts/
+fi
 docker-compose up --build
