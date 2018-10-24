919db6565f3607fb9b5da052bc39cd0920863d73
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 10:06:24 +0800

    What is love

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mkdir -p ./log/entrypoint
-echo '' > ./log/entrypoint/npm_log
-echo '' > ./log/entrypoint/yarn_log
-echo '' > ./log/entrypoint/bundle_log
-npm rebuild 2>./log/entrypoint/npm_log 1>/dev/null && yarn install 2>./log/entrypoint/yarn_log 1>/dev/null
+touch ./log/entrypoint/{yarn_log_errors,npm_log_errors,bundle_log_errors,npm_log,yarn_log,bundle_log}
+npm rebuild 2>./log/entrypoint/npm_log_errors 1>./log/entrypoint/npm_log\
+  && yarn install 2>./log/entrypoint/yarn_log_errors 1>./log/entrypoint/yarn_log
 if test -f ./tmp/pids/server.pid; then
-  rm ./tmp/pids/server.pid >/dev/null
+  rm ./tmp/pids/server.pid 1,2>/dev/null
 fi
-bundle 2>./log/entrypoint/bundle_log 1>/dev/null
+bundle 2>./log/entrypoint/bundle_log_errors 1>./log/entrypoint/bundle_log
 foreman start
