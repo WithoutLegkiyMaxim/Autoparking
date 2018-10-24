be48fcb0ed464a9f7c1888b25a8678e54d2d3e47
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 10:10:40 +0800

    Delimit npm rebuild and yarn install

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 mkdir -p ./log/entrypoint
 touch ./log/entrypoint/{yarn_log_errors,npm_log_errors,bundle_log_errors,npm_log,yarn_log,bundle_log}
 npm rebuild 2>./log/entrypoint/npm_log_errors 1>./log/entrypoint/npm_log\
-  && yarn install 2>./log/entrypoint/yarn_log_errors 1>./log/entrypoint/yarn_log
+yarn install 2>./log/entrypoint/yarn_log_errors 1>./log/entrypoint/yarn_log
 if test -f ./tmp/pids/server.pid; then
   rm ./tmp/pids/server.pid 1,2>/dev/null
 fi
