54c27e1020455b0b980b177bb9ca1cc0c559abd6
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 09:46:09 +0800

    I give you my love

--- serverUp.sh
+++ serverUp.sh
@@ -16,5 +16,8 @@ if ! test -d "./pgdata/pg_stat_tmp/"; then
   mkdir ./pgdata/pg_stat_tmp/
   touch ./pgdata/pg_stat_tmp/global.tmp
 fi
+if ! test -d "./pgdata/pg_snapshots/"; then
+  mkdir ./pgdata/pg_snapshots/
+fi
 sudo chown postgres.postgres ./pg_data/ -R
 docker-compose up --build
