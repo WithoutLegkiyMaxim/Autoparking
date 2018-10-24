3f31b7264a67c003637e99798a5d0ff35f44180c
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 09:32:45 +0800

    don't hurt me

--- serverUp.sh
+++ serverUp.sh
@@ -3,4 +3,7 @@ sudo chmod -R 777 ./pgdata
 if ! test -d "./pgdata/pg_commit_ts"; then
   mkdir ./pgdata/pg_commit_ts/
 fi
+if ! test -d "./pgdata/pg_tblspc"; then
+  mkdir ./pgdata/pg_commit_ts/
+fi
 docker-compose up --build
