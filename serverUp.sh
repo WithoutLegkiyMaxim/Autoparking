917a3bd277e23bb0b4126d9cc02c298adb6dbf41
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 09:33:45 +0800

    no more

--- serverUp.sh
+++ serverUp.sh
@@ -4,6 +4,6 @@ if ! test -d "./pgdata/pg_commit_ts"; then
   mkdir ./pgdata/pg_commit_ts/
 fi
 if ! test -d "./pgdata/pg_tblspc"; then
-  mkdir ./pgdata/pg_commit_ts/
+  mkdir ./pgdata/pg_tblspc/
 fi
 docker-compose up --build
