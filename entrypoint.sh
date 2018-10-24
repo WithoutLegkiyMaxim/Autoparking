0e075544632680a3b4faf04c6eb0232f24dcc446
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 10:20:42 +0800

    Npm, don't hurt me, please

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if test -f ./tmp/pids/server.pid; then
   rm ./tmp/pids/server.pid 1,2>/dev/null
 fi
 touch ./log/entrypoint/{yarn_log_errors,npm_log_errors,bundle_log_errors,npm_log,yarn_log,bundle_log}
-npm i 2>./log/entrypoint/npm_log_errors 1>./log/entrypoint/npm_log\
-npm rebuild 2>./log/entrypoint/npm_log_errors 1>./log/entrypoint/npm_log\
+npm i 2>./log/entrypoint/npm_log_errors 1>./log/entrypoint/npm_log
+npm rebuild 2>./log/entrypoint/npm_log_errors 1>./log/entrypoint/npm_log
 yarn install 2>./log/entrypoint/yarn_log_errors 1>./log/entrypoint/yarn_log
 bundle 2>./log/entrypoint/bundle_log_errors 1>./log/entrypoint/bundle_log
 foreman start
