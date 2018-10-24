55844235df18d37f69c0c76855805dee723c79bb
WithoutLegkiyMaxim <jamper871@gmail.com>
2018-10-24 09:48:34 +0800

    But you don't care

--- serverUp.sh
+++ serverUp.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 sudo chmod -R 777 ./pgdata
 if ! test -d "./pgdata/pg_commit_ts"; then
-  mkdir ./pgdata/pg_commit_ts/
+  mkdir -p./pgdata/pg_commit_ts/
 fi
 if ! test -d "./pgdata/pg_tblspc"; then
-  mkdir ./pgdata/pg_tblspc/
+  mkdir -p ./pgdata/pg_tblspc/
 fi
 if ! test -d "./pgdata/pg_replslot"; then
-  mkdir ./pgdata/pg_replslot/
+  mkdir -p ./pgdata/pg_replslot/
 fi
 if ! test -d "./pgdata/pg_twophase"; then
-  mkdir ./pgdata/pg_twophase/
+  mkdir -p ./pgdata/pg_twophase/
 fi
 if ! test -d "./pgdata/pg_stat_tmp/"; then
-  mkdir ./pgdata/pg_stat_tmp/
+  mkdir -p ./pgdata/pg_stat_tmp/
   touch ./pgdata/pg_stat_tmp/global.tmp
 fi
 if ! test -d "./pgdata/pg_snapshots/"; then
-  mkdir ./pgdata/pg_snapshots/
+  mkdir -p ./pgdata/pg_snapshots/
 fi
-sudo chown postgres.postgres ./pg_data/ -R
+if ! test -d "./pgdata/pg_logical/snapshots/"; then
+  mkdir -p ./pgdata/pg_logical/snapshots/
+fi
+sudo chown postgres.postgres pg_data/ -R
 docker-compose up --build
